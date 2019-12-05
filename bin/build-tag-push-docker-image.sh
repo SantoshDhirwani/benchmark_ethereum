5b2285a9ff3ff41d168265cd8c20cfeb54479d65
XhorxhinaT <56897059+XhorxhinaT@users.noreply.github.com>
2019-12-05 23:40:15 +0100

    update bash-tag-build
    building, tagging and pushing docker images into google container

--- bin/build-tag-push-docker-image.sh
+++ bin/build-tag-push-docker-image.sh
@@ -1,3 +1,31 @@
 #!/usr/bin/env bash
-
 #TODO write a script that will push all our docker images to GC so we can use them
+#Build an image from a Dockerfile-Xhorxhina
+
+#$ git clone https://github.com/Cloud-Prototyping-WS-2019-20/cp_ws_1920.git
+
+#ether-node
+#build your image locally giving a tag for its repository
+#$ docker  build /Users/[home]/cp_ws_1920/Docker/ether-node --tag ether-node
+#$ gcloud auth configure-docker
+#$ docker tag ether-node gcr.io/totemic-carrier-259013/ether-node 
+#$ docker push gcr.io/totemic-carrier-259013/ether-node
+
+
+
+
+#bootnode
+#build your image locally giving a tag for its repository
+#$ docker  build /Users/[home]/cp_ws_1920/Docker/bootnode --tag bootnode
+#$ gcloud auth configure-docker
+#$ docker tag bootnode gcr.io/totemic-carrier-259013/bootnode 
+#$ docker push gcr.io/totemic-carrier-259013/bootnode
+
+
+
+#sealer-node
+#build your image locally giving a tag for its repository
+#$ docker  build /Users/[home]/cp_ws_1920/Docker/seale-rnode --tag sealer-node
+#$ gcloud auth configure-docker
+#$ docker tag sealer-node gcr.io/totemic-carrier-259013/sealer-node
+#$ docker push gcr.io/totemic-carrier-259013/sealer-node
