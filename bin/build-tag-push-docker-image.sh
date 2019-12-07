e783efe67b4cd316757093c1c094027d7ea02f73
XhorxhinaT <56897059+XhorxhinaT@users.noreply.github.com>
2019-12-07 16:27:10 +0100

    ReUpdated build_tag_push
    Updated

--- bin/build-tag-push-docker-image.sh
+++ bin/build-tag-push-docker-image.sh
@@ -2,31 +2,31 @@
 #TODO write a script that will push all our docker images to GC so we can use them
 #Build an image from a Dockerfile-Xhorxhina
 
-$ git clone https://github.com/Cloud-Prototyping-WS-2019-20/cp_ws_1920.git
-
+cd ~/cp_ws_1920/Docker
 #ether-node
 #build your image locally giving a tag for its repository- Pleas give your local adresse here in [home]
-$ docker  build /Users/[home]/cp_ws_1920/Docker/ether-node --tag ether-node
+ docker  build ether-node  --tag ether-node
+
 #connect with you creditentials to your google cloud account, and make sure the billing is enabled
-$ gcloud auth configure-docker
-$ docker tag ether-node gcr.io/totemic-carrier-259013/ether-node 
-$ docker push gcr.io/totemic-carrier-259013/ether-node
+ gcloud auth configure-docker
+ docker tag ether-node gcr.io/totemic-carrier-259013/ether-node 
+ docker push gcr.io/totemic-carrier-259013/ether-node
 
 
 
 
 #bootnode
 #build your image locally giving a tag for its repository
-$ docker  build /Users/[home]/cp_ws_1920/Docker/bootnode --tag bootnode
-$ gcloud auth configure-docker
-$ docker tag bootnode gcr.io/totemic-carrier-259013/bootnode 
-$ docker push gcr.io/totemic-carrier-259013/bootnode
+ docker  build bootnode --tag bootnode
+ gcloud auth configure-docker
+ docker tag bootnode gcr.io/totemic-carrier-259013/bootnode 
+ docker push gcr.io/totemic-carrier-259013/bootnode
 
 
 
 #sealer-node
 #build your image locally giving a tag for its repository
-$ docker  build /Users/[home]/cp_ws_1920/Docker/seale-rnode --tag sealer-node
-$ gcloud auth configure-docker
-$ docker tag sealer-node gcr.io/totemic-carrier-259013/sealer-node
-$ docker push gcr.io/totemic-carrier-259013/sealer-node
+ docker  build sealer-node --tag sealer-node
+ gcloud auth configure-docker
+ docker tag sealer-node gcr.io/totemic-carrier-259013/sealer-node
+ docker push gcr.io/totemic-carrier-259013/sealer-node
