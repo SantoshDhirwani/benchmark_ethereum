f2e4f8a3e23a64a0e77bcd7dd07f836cf7f6c35f
eltalkarim <48909534+eltalkarim@users.noreply.github.com>
2019-12-18 19:08:13 +0100

    Put caliper in docker (#28)
    * put caliper in docker
    
    * update push image script
    
    * remove extra line

--- bin/build-tag-push-docker-image.sh
+++ bin/build-tag-push-docker-image.sh
@@ -1,29 +1,37 @@
 #!/usr/bin/env bash
-#TODO write a script that will push all our docker images to GC so we can use them
-#Before you start be sure, THAT billing is enabled for your Google Cloud project and Enable the Container Registry API.
+#Build, tag and push images to google cloud registry
 #Install and initialize the Cloud SDK using this link https://cloud.google.com/sdk/docs/?hl=de and install docker with this link https://docs.docker.com/install/linux/docker-ce/ubuntu/
 
-#Build an image from a Dockerfile-Xhorxhina
-#ether-node
-#build your image, and check it with command " docker images " to verify that the image was build
- cd ../Docker/ether-node
- docker build ether-node  --tag ether-node
+set -e
 
-#connect with you creditentials to your google cloud account, and make sure the billing is enabled
- gcloud auth configure-docker
- docker tag ether-node gcr.io/totemic-carrier-259013/ether-node 
- docker push gcr.io/totemic-carrier-259013/ether-node
+#connect with your credentials to your google cloud account
+gcloud auth configure-docker
+
+#Build, tang and push images
+#ether-node
+cd ../Docker/ether-node
+docker build --tag ether-node .
+docker tag ether-node gcr.io/totemic-carrier-259013/ether-node
+docker push gcr.io/totemic-carrier-259013/ether-node
 
 #bootnode
 #build your image locally giving a tag for its repository
- cd ../Docker/bootnode
- docker  build bootnode --tag bootnode
- docker tag bootnode gcr.io/totemic-carrier-259013/bootnode 
- docker push gcr.io/totemic-carrier-259013/bootnode
+cd ../bootnode
+docker  build --tag bootnode .
+docker tag bootnode gcr.io/totemic-carrier-259013/bootnode
+docker push gcr.io/totemic-carrier-259013/bootnode
 
 #sealer-node
 #build your image locally giving a tag for its repository
- cd ../Docker/sealer-node
- docker  build sealer-node --tag sealer-node
- docker tag sealer-node gcr.io/totemic-carrier-259013/sealer-node
- docker push gcr.io/totemic-carrier-259013/sealer-node
+cd ../sealer-node
+docker  build --tag sealer-node .
+docker tag sealer-node gcr.io/totemic-carrier-259013/sealer-node
+docker push gcr.io/totemic-carrier-259013/sealer-node
+
+#sealer-node
+cd ../caliper
+docker  build --tag caliper .
+docker tag caliper gcr.io/totemic-carrier-259013/caliper
+docker push gcr.io/totemic-carrier-259013/caliper
+
+echo all images pushed succesfully!
