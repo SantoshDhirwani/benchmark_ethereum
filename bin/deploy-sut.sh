6a9378b66037d6ee48e750f15f1e174d69f10306
Fayan Leonardo Pardo <fayanpardo@gmail.com>
2020-01-10 15:14:43 +0100

    added gaslimit edition in genesis.json

--- bin/deploy-sut.sh
+++ bin/deploy-sut.sh
@@ -24,7 +24,7 @@ sleep 60
 
 IP_BOOTNODE=$(gcloud compute instances list --filter="name~${BOOT_NODE_NAME}" --format='value(INTERNAL_IP)')
 gcloud compute ssh ${BOOT_NODE_NAME} --command "nohup bootnode -genkey boot.key && bootnode -nodekey boot.key -addr ${IP_BOOTNODE}:30310  &"
-key=$(gcloud compute ssh bootnode --command "cat boot.key")
+key=$(gcloud compute ssh ${BOOT_NODE_NAME} --command "cat boot.key")
 hex=$(gcloud compute ssh ${BOOT_NODE_NAME} --command "nohup bootnode -nodekeyhex $key -writeaddress")
 BOOTNODE_ENODE=enode://${hex}@${IP_BOOTNODE}?discport=30310
 echo $BOOTNODE_ENODE
@@ -64,6 +64,8 @@ rm -rf ~/.puppeth
 rm -f genesis.json genesis-harmony.json
 printf "2\n1\n2\n${BLOCK_INTERVAL}\n${ACCOUNT_STRING}yes\n${NETWORK_ID}\n2\n2\n\n" | puppeth --network genesis
 
+gaslimit=$(printf '%x\n' ${BLOCK_SIZE})
+jq -c ".gasLimit = \"0x${gaslimit}\"" genesis.json > tmp.$$.json && mv tmp.$$.json genesis.json
 
 for index in ${!INSTANCE_LIST[@]}; do
     echo genesis init on ${INSTANCE_LIST[index]}
@@ -71,5 +73,5 @@ for index in ${!INSTANCE_LIST[@]}; do
     gcloud compute ssh ${INSTANCE_LIST[index]} --command "geth --nousb --datadir .ethereum/ init genesis.json"
     ACCOUNT=$(gcloud compute ssh ${INSTANCE_LIST[index]} --command "geth --nousb --datadir .ethereum/ account list | cut -d "{" -f2 | cut -d "}" -f1")
     #start the node
-     gcloud compute ssh ${INSTANCE_LIST[index]} --command "geth --datadir .ethereum/ --syncmode 'full' --port 30311 --rpc --rpcaddr '0.0.0.0' --rpcport 8501 --rpcapi 'personal,db,eth,net,web3,txpool,miner' --bootnodes \"${BOOTNODE_ENODE}\" --networkid ${NETWORK_ID} --gasprice '1' -unlock ${ACCOUNT} --password password --allow-insecure-unlock --nousb --mine"
+     gcloud compute ssh ${INSTANCE_LIST[index]} --command "geth --datadir .ethereum/ --syncmode 'full' --port 30311 --rpc --rpcaddr '0.0.0.0' --rpcport 8501 --rpcapi 'personal,db,eth,net,web3,txpool,miner' --bootnodes \"${BOOTNODE_ENODE}\" --networkid ${NETWORK_ID} --gasprice '1' -unlock ${ACCOUNT} --password password --allow-insecure-unlock --nousb --mine 2> exec.log"
 done
