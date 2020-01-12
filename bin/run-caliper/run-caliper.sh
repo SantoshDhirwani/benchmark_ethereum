6269fd10353d01bddd2c2333f045d9f1babb8922
ulalaa14 <st.ardhitoprasetya@gmail.com>
2020-01-12 19:16:49 +0100

    update run-caliper.sh for installing pre-requisites and running the caliper

--- bin/run-caliper/run-caliper.sh
+++ bin/run-caliper/run-caliper.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
-cd ~
-cd hyperledger/caliper-workspace
+#pre-requisite installation
+curl -sL https://deb.nodesource.com/setup_13.x | sudo -E bash -
+sudo apt-get install -y nodejs
+npm install web3
+
+#caliper part
+npm install --only=prod @hyperledger/caliper-cli
 npx caliper bind --caliper-bind-sut ethereum --caliper-bind-sdk 1.2.1 --caliper-cwd ./ --caliper-bind-args="-g"
-npx caliper benchmark run --caliper-workspace /hyperledger/caliper-workspace --caliper-benchconfig config.yaml --caliper-networkconfig network.json
+npx caliper benchmark run --caliper-workspace ./config --caliper-benchconfig config.yaml --caliper-networkconfig network.json
