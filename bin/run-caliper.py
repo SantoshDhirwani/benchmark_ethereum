e75ec6d005e5ac689aa2064416316ab2dd33d89c
ulalaa <st.ardhitoprasetya@gmail.com>
2020-01-19 21:47:12 +0100

    add error handling in run-caliper.sh and run-caliper.py then retry

--- bin/run-caliper.py
+++ bin/run-caliper.py
@@ -2,7 +2,22 @@ import json
 import os
 import argparse
 
+CURRENT_FOLDER = os.path.dirname(os.path.abspath(__file__))
+def _get_path(filename):
+    return os.path.join(CURRENT_FOLDER, filename)
 
+def load_config(path):
+    print('Reading user configuration...')
+    with open(path) as fp:
+        config = json.load(fp)
+
+    try:
+        return config
+    except KeyError:
+        message = "You have an incorrect config structure: {}"
+        reason = "Can't load config from the 'config' folder"
+        raise KeyError(message.format(reason))
+    
 def load_args():
     parser = argparse.ArgumentParser(description="This script is for running caliper benchmark")
     #parser.add_argument("--ipaddress", help="IP Address of the node SUT", required=True)
@@ -32,6 +47,8 @@ def update_json(filename, args):
     with open('caliper-config/networks/ethereum/1node-clique/ethereum.json', "w") as jsonFile:
         json.dump(data, jsonFile, indent=4)
 
+#get config for attempt
+CONFIG_PATH = os.path.join(_get_path('../config'), 'config.json')
 
 def main():
     #load args
@@ -41,9 +58,24 @@ def main():
     update_json(networkfile, config)
 
     #run the caliper
+    config_general = load_config(CONFIG_PATH)
+    attempt = config_general['run_caliper']['attempt']
     bashfile = "run-caliper.sh"
     reportname = config.interval + "seconds-" + config.gaslimit + ".html"
-    os.system("bash " + bashfile + " " + reportname)
+    #running in number of attempts if it's failing (attempts variable getting from config)
+    for i in range(attempt):
+        #running run-caliper.sh with reportname
+        run_command = os.system("bash " + bashfile + " " + reportname)
+        if run_command == 0:
+            print("caliper success")
+            break
+
+        # if it reach the maximum attempt, printing error message
+        if i == attempt-1:
+            print("max retry error")
+        else:
+            print("Caliper retrying...")
+            pass
     exit(0)
 
 
--- bin/run-caliper.sh
+++ bin/run-caliper.sh
@@ -9,5 +9,10 @@ REPORTNAME=${1}
 #npm install web3
 #npm install --only=prod @hyperledger/caliper-cli
 #npx caliper bind --caliper-bind-sut ethereum --caliper-bind-sdk 1.2.1 --caliper-cwd ./ --caliper-bind-args="-g"
-npx caliper benchmark run --caliper-workspace caliper-config --caliper-benchconfig scenario/simple/config.yaml --caliper-networkconfig networks/ethereum/1node-clique/ethereum.json --caliper-report-path "../caliper-reports/${REPORTNAME}"
+npx caliper benchmark run \
+    --caliper-workspace caliper-config \
+    --caliper-benchconfig scenario/simple/config.yaml \
+    --caliper-networkconfig networks/ethereum/1node-clique/ethereum.json \
+    --caliper-report-path "../caliper-reports/${REPORTNAME}" \
+|| rm -r ../caliper-reports/${REPORTNAME} && exit 1 #delete report and return exit code
 exit 0
