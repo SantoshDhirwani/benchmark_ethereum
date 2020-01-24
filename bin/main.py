90cc8675a415fc253db0dc336465ab91afe10f65
Fayan Leonardo Pardo <fayanpardo@gmail.com>
2020-01-24 16:25:07 +0100

    added algorithm

--- bin/main.py
+++ bin/main.py
@@ -52,21 +52,20 @@ def run_file(file_path):
             break
 
 
-
 def check_execution(interval, gaslimit):
     run_file(['python', _get_path('get-last-throughput.py'), '--interval', str(interval), '--gaslimit',
-                   str(gaslimit)])
+              str(gaslimit)])
     tps = 0
     with open('last-tps', "r") as file:
         tps = float(file.read())
     print(tps)
-    #TODO check if last value improved enough to continue benchmarking
+    # TODO check if last value improved enough to continue benchmarking
     return 0
-  
+
 
 def find_min_interval(config):
     intervals = range(1,
-                      config['test_param']['defaultInterval'] + config['test_param']['intervalStep'],
+                      config['test_param']['maxInterval'] + config['test_param']['intervalStep'],
                       config['test_param']['intervalStep'])
     for interval in intervals:
         print('Benchmarking to find minimum value, current configuration ' + str(interval) + ' seconds and ' +
@@ -81,7 +80,7 @@ def find_min_interval(config):
             # run_file(
             #    ['sh', _get_path('test.sh'), str(interval),
             #     str(config['test_param']['defaultGas'])])
-            print('Minimum block interval found! - ' + str(interval) + ' seconds.')
+            print('Minimum block interval found! ' + str(interval) + ' seconds.')
             return interval
         except Exception as e:
             print('Failed execution with configuration %s seconds and %s gas limit. Reason: %s' % (
@@ -93,35 +92,52 @@ def find_min_interval(config):
     return -1
 
 
-def find_min_gas_limit(config):
-    gasLimit = range(1, config['test_param']['defaultGas'] + config['test_param']['gasStep'],
-                     config['test_param']['gasStep'])
-    for gas in gasLimit:
-        print('Benchmarking to find minimum value, current configuration ' + str(config['test_param'][
-            'defaultInterval']) + ' seconds and ' +
-              str(gas) + ' gas limit.')
+def find_min_gas_limit(config, interval):
+    upper_bound = config['test_param']['minGas']
+    lower_bound = upper_bound
+
+    while True:
         try:
             run_file(
                 ['sh', _get_path('deploy-sut.sh'), str(config['eth_param']['nodeNumber']),
-                 str(config['test_param']['defaultInterval']),
-                 str(gas), '0'])
-            run_file(['python', _get_path('run-caliper.py'), '--interval', str(config['test_param']['defaultInterval']),
+                 str(interval),
+                 str(upper_bound), '0'])
+            run_file(['python', _get_path('run-caliper.py'), '--interval', str(interval),
                       '--gaslimit',
-                      str(gas)])
+                      str(upper_bound)])
+            break
+        except Exception as e:
+            print('Failed execution with configuration %s seconds and %s gas limit. Reason: %s' % (
+                str(interval), str(upper_bound), e))
+        lower_bound = upper_bound
+        upper_bound *= 2
+
+    upper_bound = (upper_bound + lower_bound) / 2
+    accuracy = config["test_param"]["gasLimitAccuracy"]
+
+    while True:
+        try:
+            run_file(
+                ['sh', _get_path('deploy-sut.sh'), str(config['eth_param']['nodeNumber']),
+                 str(interval),
+                 str(upper_bound), '0'])
+            run_file(['python', _get_path('run-caliper.py'), '--interval', str(interval),
+                      '--gaslimit',
+                      str(upper_bound)])
             # UNCOMMENT ONLY FOR TESTING PURPOSES
             # run_file(
             #    ['sh', _get_path('test.sh'),
             #     str(config['test_param']['defaultInterval']), str(gas)])
-            print('Minimum block gas limit found! - ' + str(gas))
-            return gas
+            if accuracy < (abs(upper_bound-lower_bound)):
+                break
+            else:
+                upper_bound = (upper_bound + lower_bound) / 2
         except Exception as e:
             print('Failed execution with configuration %s seconds and %s gas limit. Reason: %s' % (
-                str(config['test_param']['defaultInterval']), str(gas), e))
+                str(interval), str(upper_bound), e))
+            lower_bound = (upper_bound + lower_bound) / 2
 
-    print(
-        'Minimum block gas limit not found. Check again your setup or '
-        'change the configuration values (default and steps values).')
-    return -1
+    return upper_bound
 
 
 if __name__ == '__main__':
@@ -129,13 +145,13 @@ if __name__ == '__main__':
     config = load_config(CONFIG_PATH)
 
     # Backing up old results
-    run_file(['python', _get_path('backup-old-results.py')])
+    #run_file(['python', _get_path('backup-old-results.py')])
 
     # Building SUT for the first time
-    run_file(
-        ['sh', _get_path('deploy-sut.sh'), str(config['eth_param']['nodeNumber']),
-         str(config['test_param']['defaultInterval']),
-         str(config['test_param']['defaultGas']), '1'])
+    # run_file(
+    #    ['sh', _get_path('deploy-sut.sh'), str(config['eth_param']['nodeNumber']),
+    #     str(config['test_param']['defaultInterval']),
+    #     str(config['test_param']['defaultGas']), '1'])
 
     # Finding the minimum block interval
     min_interval = find_min_interval(config)
@@ -164,7 +180,7 @@ if __name__ == '__main__':
             run_file(['sh', _get_path('deploy-sut.sh'), str(config['eth_param']['nodeNumber']), str(interval), str(gas),
                       '0'])
             run_file(['python', _get_path('run-caliper.py'), '--interval', str(interval), '--gaslimit', str(gas)])
-            #if check_execution(interval, gas) is None:
+            # if check_execution(interval, gas) is None:
             break
 
     print('Aggregating all the workload reports')
