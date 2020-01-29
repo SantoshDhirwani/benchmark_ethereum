cb4c9ec9b6e497a4c0d9595479501bddab8fd0f9
XTaraj <56897059+XhorxhinaT@users.noreply.github.com>
2020-01-29 21:01:51 +0100

    Update run-caliper.py

--- bin/workload/run-caliper.py
+++ bin/workload/run-caliper.py
@@ -2,7 +2,22 @@ import json
 import os
 import argparse
 
+CURRENT_FOLDER = os.path.dirname(os.path.abspath(__file__))
+def _get_path(filename):
+    return os.path.join(CURRENT_FOLDER, filename)
 
+def load_config(path):
+    print('Reading user configuration...')
+    with open(path) as fp:
+        config = json.load(fp)
+
+    try:
+        return config
+    except KeyError:
+        message = "You have an incorrect config structure: {}"
+        reason = "Can't load config from the 'config' folder"
+        raise KeyError(message.format(reason))
+    
 def load_args():
     parser = argparse.ArgumentParser(description="This script is for running caliper benchmark")
     #parser.add_argument("--ipaddress", help="IP Address of the node SUT", required=True)
@@ -29,21 +44,39 @@ def update_json(filename, args):
     data["ethereum"]["fromAddress"]= "0x" + instance_data[2]
     data["ethereum"]["fromAddressPassword"]= instance_data[3].split('\n')[0]
 
-    with open('caliper-config/networks/ethereum/1node-clique/ethereum.json', "w") as jsonFile:
+    with open('workload/caliper-config/networks/ethereum/1node-clique/ethereum.json', "w") as jsonFile:
         json.dump(data, jsonFile, indent=4)
 
+#get config for attempt
+CONFIG_PATH = os.path.join(_get_path('../../config'), 'config.json')
 
 def main():
     #load args
     config = load_args()
     #updpate details for network file
-    networkfile = "caliper-config/sample-network.json"
+    networkfile = "workload/caliper-config/sample-network.json"
     update_json(networkfile, config)
 
     #run the caliper
-    bashfile = "run-caliper.sh"
+    config_general = load_config(CONFIG_PATH)
+    attempt = config_general['run_caliper']['attempt']
+    bashfile = "workload/run-caliper.sh"
     reportname = config.interval + "seconds-" + config.gaslimit + ".html"
-    os.system("bash " + bashfile + " " + reportname)
+    #running in number of attempts if it's failing (attempts variable getting from config)
+    for i in range(attempt):
+        #running run-caliper.sh with reportname
+        run_command = os.system("bash " + bashfile + " " + reportname)
+        if run_command == 0:
+            print("Running caliper success.")
+            break
+
+        # if it reach the maximum attempt, printing error message
+        if i == attempt-1:
+            print("Meet maximum retry. Running caliper error.")
+            exit(-1)
+        else:
+            print("Caliper retrying...")
+            pass
     exit(0)
 
 
