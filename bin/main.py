cfd368dae3ebf115b6be14fb28df5835ffdc8e05
Fayan Leonardo Pardo <fayanpardo@gmail.com>
2020-02-02 23:38:40 +0100

    improved level 0

--- bin/main.py
+++ bin/main.py
@@ -395,17 +395,15 @@ if __name__ == '__main__':
     run_file(['python', _get_path(BACKUP_PATH)], verbose=verbose_level == VERBOSE_LEVEL_2)
 
     # Building SUT for the first time
-    if verbose_level >= VERBOSE_LEVEL_1:
-        print('Building the SUT for the first time.')
+    print('Building the SUT')
     run_file(
         ['bash', _get_path(DEPLOY_SUT_PATH), str(config['eth_param']['nodeNumber']),
          str(config['test_param']['maxInterval']),
          str(config['test_param']['defaultGas']), '1',
          '--no-user-output-enabled' if verbose_level == VERBOSE_LEVEL_0 else ''],
         verbose=verbose_level >= VERBOSE_LEVEL_2)
-    if verbose_level >= VERBOSE_LEVEL_1:
-        print('SUT successfully built for the first time')
-
+    print('SUT successfully built')
+    print('Starting calculation of optimal block interval and block gas limit for maximum throughput')
     result = find_optimal_parameters()
     print("Best result found: " + str(result))
     if verbose_level >= VERBOSE_LEVEL_1:
