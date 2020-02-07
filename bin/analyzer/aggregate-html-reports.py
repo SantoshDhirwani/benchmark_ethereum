9c3de080087ca219f1385cad9a5a226334355240
ankushshr1993 <ankush.sharma@campus.tu-berlin.de>
2020-02-07 14:12:12 +0100

    Adding bubble plots and line graph

--- bin/analyzer/aggregate-html-reports.py
+++ bin/analyzer/aggregate-html-reports.py
@@ -1,45 +1,127 @@
 import glob
 import pandas as pd
+import re
+import os
+import argparse
+from shutil import copy
+import plotly.graph_objects as go
+import plotly
+import matplotlib.pyplot as plt
 
-reportsDir = 'workload/caliper-reports/*.html'
-resultsDir = 'analyzer/aggregated-results/'
+ANALYZER_PATH = "analyzer/"
+WORKLOAD_PATH = "workload/"
+reportsDir = WORKLOAD_PATH + 'caliper-reports/*.html'
+resultsDir = ANALYZER_PATH + 'aggregated-results/'
+html_template = ANALYZER_PATH + 'dashboard.html'
+html_result = ANALYZER_PATH + 'aggregated-results/dashboard.html'
+copy(html_template, html_result)
 files = glob.glob(reportsDir)
 
-my_tables = []
-for file in files:     
-    tables = pd.read_html(file)
-    temp = tables[0]
-    temp['fileName'] = file.split('/')[-1]
-    print(temp['fileName'])
-    my_tables.append(temp)
-temp = my_tables[0][['Name']].values.tolist()	
-name = my_tables[0][['Name']]	
-final = my_tables[0].drop(1)
-final['ExperimentNo'] = 0
-final['gasLimit'] = str(final['fileName']).split('-')[1].split('.')[0]
-final['gasLimit'] = final['gasLimit'].astype(float)
-final['blockInterval'] = str(final['fileName'][0]).split('second')[0]
-final['blockInterval'] = final['blockInterval'].astype(float)
-for j in range(1,len(my_tables)):
-    temp = my_tables[j].drop(1)
-    temp['ExperimentNo'] = j
-    temp['gasLimit'] = str(temp['fileName']).split('-')[1].split('.')[0]
-    temp['gasLimit'] = temp['gasLimit'].astype(float)
-    temp['blockInterval'] = str(temp['fileName'][0]).split('second')[0]
-    temp['blockInterval'] = temp['blockInterval'].astype(float)
-    final = pd.concat([final,temp], ignore_index=False, sort=False)
+def load_args():
+    parser = argparse.ArgumentParser(description="This script is for running caliper benchmark")
+    parser.add_argument("--interval", help="Optimal Block interval", required=True)
+    parser.add_argument("--gaslimit", help="Optimal Block gas limit", required=True)
+    parser.add_argument("--throughput", help="Max thoughput", required=True)
+    parser.add_argument("--executiontime", help="Execution Time", required=True)
+    return parser.parse_args()
 
-tempcheck=(final.groupby(final['Throughput (TPS)']))
+if __name__ == '__main__':
+    config = load_args()
+    my_tables = []
+    for file in files:
+        tables = pd.read_html(file)
+        temp = tables[0]
+        temp['fileName'] = file.split('/')[-1]
+        print(temp['fileName'])
+        my_tables.append(temp)
+    temp = my_tables[0][['Name']].values.tolist()
+    name = my_tables[0][['Name']]
+    final = my_tables[0].drop(1)
+    final['ExperimentNo'] = 0
+    final['gasLimit'] = str(final['fileName']).split('-')[1].split('.')[0]
+    final['gasLimit'] = final['gasLimit'].astype(float)
+    final['blockInterval'] = str(final['fileName'][0]).split('second')[0]
+    final['blockInterval'] = final['blockInterval'].astype(float)
+    for j in range(1, len(my_tables)):
+        temp = my_tables[j].drop(1)
+        temp['ExperimentNo'] = j
+        temp['gasLimit'] = str(temp['fileName']).split('-')[1].split('.')[0]
+        temp['gasLimit'] = temp['gasLimit'].astype(float)
+        temp['blockInterval'] = str(temp['fileName'][0]).split('second')[0]
+        temp['blockInterval'] = temp['blockInterval'].astype(float)
+        final = pd.concat([final, temp], ignore_index=False, sort=False)
 
-dat = pd.DataFrame()
-for key, item in tempcheck:
-    dat=pd.concat([dat,tempcheck.get_group(key)], ignore_index=True)
-#create overall report
-dat.to_csv(resultsDir + 'data.csv',index=False)
+    final = final.drop('Succ', axis=1).drop('Fail', axis=1).drop('Send Rate (TPS)', axis=1).drop(
+        'Max Latency (s)', axis=1).drop(
+        'Min Latency (s)', axis=1).drop('Avg Latency (s)', axis=1).drop('fileName', axis=1).drop('ExperimentNo',
+                                                                                                 axis=1)
+    final.rename(columns={'Throughput (TPS)': 'throughput'}, inplace=True)
+    tempcheck = final.groupby(final['throughput'])
 
-#create seperate fille for each function
-for name in dat['Name'].unique():
-    file_name = resultsDir + 'data_{0}.csv'.format(name)
-    dat[dat['Name']==name].to_csv(file_name,index=False)
+    dat = pd.DataFrame()
+    for key, item in tempcheck:
+        dat = pd.concat([dat, tempcheck.get_group(key)], ignore_index=True)
 
-exit(0)
+    # create overall report
+
+    dat.to_csv(resultsDir + 'data.csv', index=False)
+    html = ''
+    # create seperate fille for each function
+    for name in dat['Name'].unique():
+        file_name_csv = resultsDir + 'data_{0}.csv'.format(name)
+        file_name_html = resultsDir + 'data_{0}.html'.format(name)
+        dat[dat['Name'] == name].drop('Name', axis=1).to_csv(file_name_csv, index=False)
+        df = dat[dat['Name'] == name].drop('Name', axis=1)
+        html = dat[dat['Name'] == name].drop('Name', axis=1).to_html(index=False)
+
+    html = html.split('\n', 3)[3]
+    #Create plots for report
+    data = dat
+    print(data.columns)
+    data = data.loc[data['Name']=='transfer']
+    gaslimit = data['gasLimit'].values
+    blockinterval = data['blockInterval'].values
+    tps = data['throughput'].values
+    fig = go.Figure(data=[go.Scatter(
+        x=gaslimit,
+        y=blockinterval,
+        text=['TPS:'+str(s) for s in tps],
+        mode='markers',
+        marker=dict(
+            size=tps,
+            sizemode='area',
+            sizeref=2.*max(tps)/(40.**2),
+            sizemin=4
+            )
+            )])
+    fig.update_layout(
+    title="Throughput for varying gaslimit and blockintervakl",
+    xaxis_title="Gaslimit",
+    yaxis_title="Block interval",
+    font=dict(
+    family="Courier New, monospace",
+    size=18,
+    color="#7f7f7f"
+    )
+    )
+    plotly.offline.plot(fig, filename='bubbleplot.html', auto_open=False)
+    fig, ax = plt.subplots(1,1);
+    data.groupby("blockInterval").plot(x="gasLimit", y="throughput", ax=ax)
+    plt.xlabel('Gaslimit')
+    plt.ylabel('Throughput')
+    plt.legend([v[0] for v in data.groupby('blockInterval')['blockInterval']], title = 'Block interval')
+    plt.savefig('line_graph.png')
+
+    # print(html)
+    with open(html_result, "r+") as f:
+        data = f.read()
+        data = data.replace("{table}", html).replace("{interval}", config.interval).replace("{gaslimit}",
+                                                                                            config.gaslimit).replace(
+            "{throughput}", config.throughput)
+        f.seek(0)
+        f.write(data)
+        f.truncate()
+
+    #run_command = os.system("Rscript " + r_script)
+
+    exit(0)
